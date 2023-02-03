e95ea68be1bb1ed7747c7ab512c9de58c3a3d478
evele <ee230302@gmail.com>
2023-02-03 19:13:05 +0200

    Routes renamed

--- api/routes/api.php
+++ api/routes/api.php
@@ -4,28 +4,28 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
-Route::post('/user/register', [AuthController::class, 'createUser']);
-Route::post('/user/login', [AuthController::class, 'loginUser'])->name('login');
+Route::post('/users/register', [AuthController::class, 'createUser']);
+Route::post('/users/login', [AuthController::class, 'loginUser'])->name('login');
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
 
-  Route::get('/item/get', function(Request $request) {
+  Route::get('/items/get', function(Request $request) {
     $owner_id = auth('sanctum')->user()->id;
-    $data = DB::table('item')->where('owner_id', $owner_id)->get();
+    $data = DB::table('items')->where('owner_id', $owner_id)->get();
     return $data;
   });
 
-  Route::post('/item/register', function(Request $request) {
+  Route::post('/items/register', function(Request $request) {
     $params = $request->only('name', 'quantity', 'available', 'description');
 
     $rules = [
-      'name' => 'required|max:255|unique:item',
+      'name' => 'required|max:255|unique:items',
       'quantity' => 'required|numeric',
       'available' => 'required|boolean',
       'description' => 'required|max:255'
     ];
 
-    $validator = Validator::make($credentials, $rules);
+    $validator = Validator::make($params, $rules);
     
     if($validator->fails()) {
       return response()->json(['success'=> false, 'error'=> $validator->messages()]);
@@ -37,7 +37,7 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     $available = $request->available;
     $description = $request->description;
 
-    DB::table('item')->insert([
+    DB::table('items')->insert([
       'name' => $name,
       'quantity' => $quantity,
       'owner_id' => $owner_id,
@@ -52,9 +52,10 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
   });
 
-  Route::post('/item/update', function(Request $request) {
+
+  Route::post('/items/update', function(Request $request) {
     $params = $request->only('id', 'name', 'quantity', 'available', 'description');
-    $record_exisist = DB::table('item')->where('id', $request->id)->exists();
+    $record_exisist = DB::table('items')->where('id', $request->id)->exists();
 
     if(!$record_exisist) {
       return response()->json([
@@ -65,13 +66,13 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     $rules = [
       'id' => 'required|numeric',
-      'name' => 'max:255|unique:item',
+      'name' => 'max:255|unique:items',
       'quantity' => 'nullable|numeric',
       'available' => 'nullable|boolean',
       'description' => '|max:255'
     ];
 
-    $validator = Validator::make($credentials, $rules);
+    $validator = Validator::make($params, $rules);
     
     if($validator->fails()) {
       return response()->json(['success'=> false, 'error'=> $validator->messages()]);
@@ -80,13 +81,13 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     $name = $request->name;
     $quantity = $request->quantity;
     $owner_id = auth('sanctum')->user()->id;
-    $item_owner_id = DB::table('item')->select('owner_id')->where('id', $request->id)->first()->owner_id;
+    $item_owner_id = DB::table('items')->select('owner_id')->where('id', $request->id)->first()->owner_id;
     $available = $request->available;
     $description = $request->description;
     if($owner_id == $item_owner_id) {
 
-      $updated_row = DB::table('item')->select('*')->where('id', $request->id)->first();
-      $updated_record = DB::table('item')->where('id', $request->id)->where('owner_id', $owner_id)->update([
+      $updated_row = DB::table('items')->select('*')->where('id', $request->id)->first();
+      $updated_record = DB::table('items')->where('id', $request->id)->where('owner_id', $owner_id)->update([
         'name' => ($name == null) ? $updated_row->NAME : $name,
         'quantity' => ($quantity == null) ? $updated_row->QUANTITY : $quantity,
         'available' => ($available == null) ? $updated_row->AVAILABLE: $available,
@@ -108,14 +109,14 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
     
   });
 
-  Route::post('/item/delete', function(Request $request) {
+  Route::post('/items/delete', function(Request $request) {
     $params = $request->only('id');
-    $record_exisist = DB::table('item')->where('id', $request->id)->exists();
+    $record_exisist = DB::table('items')->where('id', $request->id)->exists();
     $owner_id = auth('sanctum')->user()->id;
-    $item_owner_id = DB::table('item')->select('owner_id')->where('id', $request->id)->first()->owner_id;
+    $item_owner_id = DB::table('items')->select('owner_id')->where('id', $request->id)->first()->owner_id;
    
     if($owner_id == $item_owner_id) {
-      DB::table('item')
+      DB::table('items')
       ->where('id', $request->id)
       ->where('owner_id', $owner_id)
       ->delete();
